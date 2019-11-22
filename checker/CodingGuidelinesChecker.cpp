1c048bad2f84b792bb397c7a40bf76cef2e01e64
Dirk Reinbach <reinbach@volumegraphics.com>
2019-11-22 15:50:52 +0000

    FB-29: CMake => C++
    svn path=/work/branches/CleanCode/; revision=173104

---
+++ checker/CodingGuidelinesChecker.cpp
@@ -0,0 +1,505 @@
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wtype-limits"
+#endif
+
+#include <fmt/format.h>
+#include <lyra/lyra.hpp>
+#include <nlohmann/json.hpp>
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+#include <chrono>
+#include <fstream>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <optional>
+#include <regex>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using fmt::operator""_a;
+
+namespace {
+
+template <class... Args>
+decltype(auto) launch_async(Args&&... args)
+{
+  return std::async(std::launch::async, std::forward<Args>(args)...);
+}
+
+template <class Stream, class... Args>
+void print(Stream&& out, std::string_view fmt, Args&&... args)
+{
+  fmt::format_to(std::ostream_iterator<char>(out), fmt, std::forward<Args>(args)...);
+}
+
+// will become available in C++ 20
+struct osyncstream : public std::ostringstream
+{
+  explicit osyncstream(std::ostream& output_stream)
+  : output(output_stream)
+  {
+  }
+  ~osyncstream()
+  {
+    emit();
+  }
+
+  void emit()
+  {
+    std::ostringstream const tmp{ std::move(*this) };
+
+    static std::unordered_map<std::ostream*, std::mutex> locks;
+
+    auto const locked = std::scoped_lock{ locks[&output] };
+    output << tmp.str();
+  }
+
+private:
+  std::ostream& output;
+};
+
+template <class T>
+struct duration_scope
+{
+  duration_scope(std::string_view format)
+  : format(format)
+  , started(std::chrono::system_clock::now())
+  {
+  }
+
+  duration_scope(duration_scope&&) = delete;
+  duration_scope(duration_scope const&) = delete;
+
+  ~duration_scope()
+  {
+    auto const elapsed = std::chrono::system_clock::now() - started;
+    print(osyncstream{ std::cerr }, format, std::chrono::duration_cast<T>(elapsed).count());
+  }
+
+private:
+  std::string format;
+  std::chrono::system_clock::time_point started;
+};
+
+auto content(std::istream& in)
+{
+  std::stringstream sstr;
+  sstr << in.rdbuf();
+  return sstr.str();
+}
+
+auto read_from(std::string const& file_name)
+{
+  struct temporary_stream
+  {
+    explicit temporary_stream(std::string const& file_name)
+    : stream(file_name)
+    {
+    }
+    operator std::istream&() &&
+    {
+      return stream;
+    }
+
+  private:
+    std::ifstream stream;
+  };
+
+  return temporary_stream{ file_name };
+}
+
+auto make_string_view(std::ssub_match const& match)
+{
+  return std::string_view{ &*match.first, static_cast<size_t>(match.length()) };
+}
+
+struct coding_guidelines
+{
+  struct identifier
+  {
+    explicit identifier(std::string const& str)
+    : origin(str)
+    , prefix(origin)
+    , nr()
+    {
+      std::smatch match;
+      if (!std::regex_match(origin, match, std::regex{ "^([^\\d]+)(\\d{1,5})$" }))
+        return;
+
+      prefix = make_string_view(match[1]);
+      nr = std::stoi(match[2].str());
+    }
+
+    operator std::string_view() const
+    {
+      return origin;
+    }
+
+    bool operator<(identifier const& other) const
+    {
+      if (auto const prefix_comparision = prefix.compare(other.prefix); prefix_comparision != 0)
+        return prefix_comparision < 0;
+
+      return nr < other.nr;
+    }
+
+  private:
+    std::string origin;
+    std::string_view prefix;
+    std::optional<int> nr;
+  };
+
+  struct rule
+  {
+    std::string severity;
+    std::string summary;
+    std::string rationale;
+    std::string workaround;
+    std::regex matched_files;
+    std::regex ignored_files;
+    std::regex matched_text;
+    std::regex ignored_text;
+    std::regex marked_text;
+  };
+
+  using rule_map = std::map<identifier, rule>;
+
+  std::string origin;
+  rule_map rules;
+};
+
+void from_json(nlohmann::json const& json, coding_guidelines::rule& rule)
+{
+  rule.severity = json.at("severity");
+  rule.summary = json.at("summary");
+  rule.rationale = json.at("rationale");
+  rule.workaround = json.at("workaround");
+  rule.matched_files = std::regex{ json.at("matched_files").get<std::string>(), std::regex::optimize };
+  rule.ignored_files = std::regex{ json.at("ignored_files").get<std::string>(), std::regex::optimize };
+  rule.matched_text = std::regex{ json.at("matched_text").get<std::string>(), std::regex::optimize };
+  rule.ignored_text = std::regex{ json.at("ignored_text").get<std::string>(), std::regex::optimize };
+  rule.marked_text = std::regex{ json.at("marked_text").get<std::string>(), std::regex::optimize };
+}
+
+auto read_coding_guidelines_from(std::string const& file_name)
+{
+  auto json = nlohmann::json{};
+  read_from(file_name) >> json;
+
+  return coding_guidelines{ file_name, json.get<coding_guidelines::rule_map>() };
+}
+
+auto async_coding_guidelines_from(std::string const& file_name)
+{
+  return launch_async([file_name] { return read_coding_guidelines_from(file_name); });
+}
+
+template <class Op>
+void for_each_line(std::istream& is, Op&& operation)
+{
+  for (std::string line; std::getline(is, line);)
+    operation(line);
+}
+
+template <class Op>
+auto make_async(Op&& operation)
+{
+  return [tasks = std::vector<std::future<void>>{}, operation = std::forward<Op>(operation)](auto&&... args) mutable {
+    tasks.emplace_back(launch_async([operation, args = std::make_tuple(std::forward<decltype(args)>(args)...)] {
+      std::apply(operation, std::move(args));
+    }));
+  };
+}
+
+struct as_literal
+{
+  std::string_view str;
+};
+} // namespace
+
+namespace fmt {
+
+template <>
+struct formatter<as_literal>
+{
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx)
+  {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(as_literal const& text, FormatContext& ctx)
+  {
+    auto out = ctx.out();
+    for (auto it = text.str.begin(); it != text.str.end(); ++it)
+    {
+      switch (*it)
+      {
+      case '\n':
+        out = base.format('\\', ctx);
+        out = base.format('n', ctx);
+        break;
+      case '\\':
+        [[fallthrough]];
+      case '\"':
+        out = base.format('\\', ctx);
+        [[fallthrough]];
+      default:
+        out = base.format(*it, ctx);
+        break;
+      }
+    }
+
+    return out;
+  }
+
+private:
+  formatter<char> base;
+};
+
+} // namespace fmt
+
+namespace {
+
+auto count(std::string_view str, char ch)
+{
+  return std::count(begin(str), end(str), ch);
+}
+
+auto async_content_from(std::string const& file_name)
+{
+  return launch_async([file_name]() { return content(read_from(file_name)); });
+}
+
+auto check_rule_in(std::string const& file_name)
+{
+  auto const content = async_content_from(file_name).share();
+
+  return [file_name, content](coding_guidelines const& guidelines, auto const& id, auto const& rule) {
+    if (!std::regex_search(file_name, rule.matched_files))
+      return fmt::format("\n// rule {} not matched\n", id);
+
+    if (std::regex_search(file_name, rule.ignored_files))
+      return fmt::format("\n// rule {} ignored\n", id);
+
+    auto out = std::ostringstream{};
+    print(out, "\n// rule {} checked\n", id);
+
+    ptrdiff_t nr = 0;
+
+    std::for_each(
+      std::sregex_iterator{ begin(content.get()), end(content.get()), rule.matched_text },
+      std::sregex_iterator{},
+      [&](auto const& sub_match) {
+        auto const prefix = make_string_view(sub_match.prefix());
+        auto const suffix = make_string_view(sub_match.suffix());
+
+        auto const last_newline_pos = prefix.find_last_of('\n');
+        auto const first_newline_pos = suffix.find_first_of('\n');
+
+        auto const match_prefix =
+          (last_newline_pos == std::string_view::npos) ? prefix : prefix.substr(last_newline_pos + 1);
+
+        auto const match_suffix =
+          (first_newline_pos == std::string_view::npos) ? suffix : suffix.substr(0, first_newline_pos);
+
+        auto const match = std::string_view{ &*(sub_match[0].first), static_cast<size_t>(sub_match.length()) };
+
+        std::string indent;
+        std::string marker;
+        std::smatch marked;
+
+        if (std::regex_search(sub_match[0].first, sub_match[0].second, marked, rule.marked_text))
+        {
+          indent.resize(match_prefix.length() + marked.prefix().length(), ' ');
+          marker.resize(marked.length(), '~');
+        }
+
+        std::string source;
+        source.append(match_prefix);
+        source.append(match);
+        source.append(match_suffix);
+
+        auto const ignored = std::regex_search(source, rule.ignored_text) ? "// Ignored: " : "";
+
+        // FIXME: marker, wenn match über mehrere zeilen geht
+
+        nr += count(prefix, '\n');
+
+        print(
+          out,
+          R"message(
+{ignored}#if defined(__GNUC__)
+{ignored}# line {nr} "{file_name}"
+{ignored}# pragma GCC warning \
+{ignored}{indent}"{id}: {summary} [{origin}]\n SEVERITY  : {severity}\n RATIONALE : {rationale}\n WORKAROUND: {workaround}"
+{ignored}#elif defined(_MSC_VER)
+{ignored}# line {nr} "{file_name}"
+{ignored}
+{ignored}# pragma message(__FILE__ "(" STRINGIFY(__LINE__) "): warning: {id}: {summary} [{origin}]\n SEVERITY  : {severity}\n RATIONALE : {rationale}\n WORKAROUND: {workaround}\n {source}\n {indent}{marker}")
+{ignored}#endif
+)message",
+          "ignored"_a = ignored,
+          "indent"_a = indent,
+          "marker"_a = marker,
+          "nr"_a = nr,
+          "file_name"_a = as_literal{ file_name },
+          "source"_a = as_literal{ source },
+          "id"_a = as_literal{ id },
+          "origin"_a = as_literal{ guidelines.origin },
+          "summary"_a = as_literal{ rule.summary },
+          "severity"_a = as_literal{ rule.severity },
+          "rationale"_a = as_literal{ rule.rationale },
+          "workaround"_a = as_literal{ rule.workaround });
+
+        nr += count(match, '\n');
+      });
+
+    return out.str();
+  };
+}
+
+auto async_messages_from(coding_guidelines const& guidelines, std::string const& file_name)
+{
+  auto const check_rule_in_file = check_rule_in(file_name);
+
+  auto messages = std::vector<std::future<std::string>>{};
+
+  for (auto const& [id, rule] : guidelines.rules)
+    messages.push_back(launch_async(check_rule_in_file, guidelines, id, rule));
+
+  return messages;
+}
+
+template <class Guidelines, class Filter>
+auto check_file(Guidelines&& guidelines, Filter&& filter)
+{
+  return [guidelines=std::forward<Guidelines>(guidelines), filter=std::forward<Filter> (filter)](std::string const& file_name) {
+
+    if (!std::invoke(filter, file_name))
+      return;
+
+    auto messages = async_messages_from(guidelines.get(), file_name);
+
+    auto synchronized_out = osyncstream{ std::cout };
+    print(synchronized_out, "\n// {}\n", file_name);
+
+    for (auto&& message : messages)
+      synchronized_out << message.get();
+  };
+}
+
+struct parameters
+{
+  std::string coding_guidelines;
+  std::string source_files;
+  std::string files_to_check;
+  std::string output_file;
+
+  static auto parse(int argc, char* argv[])
+  {
+    parameters p;
+
+    auto const cli =
+      lyra::arg(p.coding_guidelines, "codingGuidelines")("Coding guidelines (JSON file) to check") |
+      lyra::arg(p.source_files, "source files")("File with list of source files to scan") |
+      lyra::opt(p.files_to_check, "files to check")["-f"]["--files-to-check"]("File with list of files to check") |
+      lyra::opt(p.output_file, "output file")["-o"]["--output-file"]("Output file name");
+
+    auto const result = cli.parse({ argc, argv });
+    if (!result)
+      throw std::invalid_argument{ "Error in command line: " + result.errorMessage() };
+
+    return p;
+  }
+};
+
+auto read_filter_from(std::string const& files_to_check)
+{
+  auto file_names = std::optional<std::unordered_set<std::string>>{};
+
+  if (!files_to_check.empty())
+  {
+    file_names.emplace();
+    for_each_line(read_from(files_to_check), [&file_names](auto const& file_name) { file_names->emplace(file_name); });
+  }
+
+  return [file_names = std::move(file_names)](auto const& file_name) {
+    return !file_names || file_names->count(file_name) != 0;
+  };
+}
+
+void print_header()
+{
+  std::cout << R"header(// DO NOT EDIT: this file is generated automatically
+
+namespace { using symbol = int; }
+
+#define __STRINGIFY(x) #x
+#define STRINGIFY(x) __STRINGIFY(x)
+)header";
+}
+
+auto redirect_cout_to(std::string const& file_name)
+{
+  struct redirection
+  {
+    redirection(std::string const& file_name)
+    : stream(file_name)
+    , backup(std::cout.rdbuf(stream.rdbuf()))
+    {
+    }
+    ~redirection()
+    {
+      std::cout.rdbuf(backup);
+    }
+
+  private:
+    std::ofstream stream;
+    std::streambuf* backup;
+  };
+
+  if (file_name.empty())
+    return std::unique_ptr<redirection>{};
+
+  return std::make_unique<redirection>(file_name);
+}
+
+} // namespace
+
+int main(int argc, char* argv[])
+{
+  auto const overall_duration = duration_scope<std::chrono::milliseconds>{ "Elapsed time in milliseconds: {}\n" };
+
+  std::ios::sync_with_stdio(false);
+
+  try
+  {
+    auto const parameters = parameters::parse(argc, argv);
+
+    auto const guidelines = async_coding_guidelines_from(parameters.coding_guidelines).share();
+
+    // FIXME: async filter
+    auto const filter = read_filter_from(parameters.files_to_check);
+
+    auto const output = redirect_cout_to(parameters.output_file);
+
+    print_header();
+    for_each_line(read_from(parameters.source_files), make_async(check_file(guidelines, filter)));
+  }
+  catch (std::exception const& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+
+  return 0;
+}
