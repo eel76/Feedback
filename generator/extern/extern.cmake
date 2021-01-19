bfcc9aa7d62a314af72e47eae6bc32b9cae48ade
Dirk Reinbach <dirk.reinbach@hexagon.com>
2021-01-19 18:40:55 +0100

    Refactor CMake code.

--- generator/extern/extern.cmake
+++ generator/extern/extern.cmake
@@ -1,46 +1,60 @@
 include(FetchContent)
 
-function (Extern_FindTargets targets_variable directory)
-  get_directory_property(targets DIRECTORY ${directory} BUILDSYSTEM_TARGETS)
-  get_directory_property(subdirectories DIRECTORY ${directory} SUBDIRECTORIES)
+function (Extern_FindTargets targets_variable)
+  cmake_parse_arguments (parameter "" "IMPORTED" "DIRECTORIES;TYPES" ${ARGN})
 
-  foreach (subdirectory IN LISTS subdirectories)
-    Extern_FindTargets (subdirectory_targets ${subdirectory})
-    list (APPEND targets ${subdirectory_targets})
-  endforeach ()
-
-  set (${targets_variable} ${targets} PARENT_SCOPE)
-endfunction ()
-
-function (Extern_MakeAvailable package git_repository git_tag)
-  cmake_parse_arguments(MakeAvailable "" "BUILD_TESTING_VARIABLE;SOURCE_DIR_VARIABLE" "" ${ARGN})
-
-  if (DEFINED MakeAvailable_UNPARSED_ARGUMENTS)
-    message (FATAL_ERROR "Unparsed arguments: ${MakeAvailable_UNPARSED_ARGUMENTS}")
+  if (DEFINED parameter_UNPARSED_ARGUMENTS)
+    message (FATAL_ERROR "Unparsed arguments: ${parameter_UNPARSED_ARGUMENTS}")
   endif ()
 
-  if (DEFINED MakeAvailable_BUILD_TESTING_VARIABLE)
-    set(${MakeAvailable_BUILD_TESTING_VARIABLE} OFF CACHE INTERNAL "")
+  if (NOT DEFINED parameter_DIRECTORIES)
+    message (FATAL_ERROR "No directories given.")
   endif ()
 
-  FetchContent_Declare(
-    ${package}
-    GIT_REPOSITORY ${git_repository}
-    GIT_TAG ${git_tag}
-    GIT_SHALLOW TRUE
-    )
-  FetchContent_MakeAvailable(${package})
+  if (NOT DEFINED parameter_IMPORTED)
+    set (parameter_IMPORTED FALSE)
+  endif ()
 
-  Extern_FindTargets (targets "${${package}_SOURCE_DIR}")
+  if (NOT DEFINED parameter_TYPES)
+    set (parameter_TYPES STATIC_LIBRARY MODULE_LIBRARY SHARED_LIBRARY EXECUTABLE)
+  endif ()
 
-  foreach (target IN LISTS targets)
-    get_target_property (type ${target} TYPE)
-    if (" STATIC_LIBRARY MODULE_LIBRARY SHARED_LIBRARY EXECUTABLE " MATCHES " ${type} ")
-      set_target_properties (${target} PROPERTIES FOLDER "extern/${package}")
+  list(JOIN parameter_TYPES " " types)
+  unset (targets)
+
+  foreach (directory IN LISTS parameter_DIRECTORIES)
+    get_directory_property(buildsystem_targets DIRECTORY "${directory}" BUILDSYSTEM_TARGETS)
+
+    foreach (target IN LISTS buildsystem_targets)
+      get_target_property (type "${target}" TYPE)
+      get_target_property (imported "${target}" IMPORTED)
+
+      if (" ${types} " MATCHES " ${type} " AND (NOT imported OR parameter_IMPORTED))
+        list (APPEND targets "${target}")
+      endif ()
+    endforeach ()
+
+    get_directory_property(subdirectories DIRECTORY "${directory}" SUBDIRECTORIES)
+
+    if (subdirectories)
+      Extern_FindTargets (subdirectory_targets DIRECTORIES ${subdirectories} TYPES ${parameter_TYPES})
+      list (APPEND targets ${subdirectory_targets})
     endif ()
   endforeach ()
 
-  if (DEFINED MakeAvailable_SOURCE_DIR_VARIABLE)
-    set (${MakeAvailable_SOURCE_DIR_VARIABLE} ${${package}_SOURCE_DIR} PARENT_SCOPE)
+  set ("${targets_variable}" ${targets} PARENT_SCOPE)
+endfunction ()
+
+function (Extern_MakeAvailable package)
+  cmake_parse_arguments(parameter "" "SOURCE_DIR_VARIABLE" "" ${ARGN})
+
+  FetchContent_Declare("${package}" ${parameter_UNPARSED_ARGUMENTS})
+  FetchContent_MakeAvailable("${package}")
+
+  Extern_FindTargets (extern_targets DIRECTORIES "${${package}_SOURCE_DIR}")
+  set_target_properties (${extern_targets} PROPERTIES FOLDER "extern/${package}")
+
+  if (DEFINED parameter_SOURCE_DIR_VARIABLE)
+    set ("${parameter_SOURCE_DIR_VARIABLE}" "${${package}_SOURCE_DIR}" PARENT_SCOPE)
   endif ()
 endfunction ()
