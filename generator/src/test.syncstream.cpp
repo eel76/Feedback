babcd72b46f04978462d5bab0da0d311319a13a7
Dirk Reinbach <dirk.reinbach@hexagon.com>
2021-01-28 22:13:42 +0100

    Extend test scenario.

--- generator/src/test.syncstream.cpp
+++ generator/src/test.syncstream.cpp
@@ -5,39 +5,61 @@
 #include <sstream>
 
 SCENARIO("osyncstream usage", "[syncstream]") {
-  GIVEN("A synchronized output stream") {
+  GIVEN("An output stream") {
     std::ostringstream out;
 
-    auto synchronized_out = std::make_unique<cxx20::osyncstream>(out);
+    AND_GIVEN("a synchronized stream on it") {
+      auto synchronized_out = std::make_unique<cxx20::osyncstream>(out);
 
-    WHEN("something is written to it") {
-      *synchronized_out << "something";
+      WHEN("something is written to it") {
+        *synchronized_out << "something";
 
-      THEN("something is not emitted yet") {
-        REQUIRE(out.str() == "");
+        THEN("something is not emitted yet") {
+          REQUIRE(out.str() == "");
 
-        AND_WHEN("emit is requested") {
-          synchronized_out->emit();
+          AND_WHEN("emit is requested") {
+            synchronized_out->emit();
 
-          THEN("something is emitted") {
-            REQUIRE(out.str() == "something");
+            THEN("something is emitted") {
+              REQUIRE(out.str() == "something");
 
-            AND_WHEN("something else is written to it") {
-              *synchronized_out << " else";
+              AND_WHEN("something else is written to it") {
+                *synchronized_out << " else";
 
-              THEN("something else is not emitted yet") {
-                REQUIRE(out.str() == "something");
+                THEN("something else is not emitted yet") {
+                  REQUIRE(out.str() == "something");
 
-                AND_WHEN("it is destroyed") {
-                  synchronized_out.reset();
+                  AND_WHEN("it is destroyed") {
+                    synchronized_out.reset();
 
-                  THEN("something else is emitted") {
-                    REQUIRE(out.str() == "something else");
+                    THEN("something else is emitted") {
+                      REQUIRE(out.str() == "something else");
+                    }
                   }
                 }
               }
             }
           }
+        }
+      }
+    }
+  }
+
+  GIVEN("An output stream") {
+    std::ostringstream out;
+
+    THEN("it is empty") {
+      REQUIRE(out.str() == "");
+    }
+
+    AND_GIVEN("a synchronized stream on it") {
+      auto synchronized_out = std::make_unique<cxx20::osyncstream>(out);
+
+      WHEN("the synchronized stream is destroyed") {
+        synchronized_out.reset();
+
+        THEN("the output stream is still empty") {
+          REQUIRE(out.str() == "");
         }
       }
     }
