a4185b3674213e20629533540369c2b0029930d3
Dirk Reinbach <dirk.reinbach@hexagon.com>
2021-02-14 21:33:55 +0100

    Add quick'n'dirty support for better compiler messages.

--- generator/core/include/generator/format.h
+++ generator/core/include/generator/format.h
@@ -10,31 +10,42 @@ namespace generator::format {
     fmt::format_to(std::ostream_iterator<char>(out), fmt, std::forward<Args>(args)...);
   }
 
-  struct as_literal {
+  struct as_compiler_message {
     std::string_view str;
   };
-
-  struct uppercase {
-    std::string_view str;
-  };
-
 } // namespace generator::format
 
 namespace fmt {
 
-  template <> struct formatter<generator::format::as_literal> {
+  template <> struct formatter<generator::format::as_compiler_message> {
     template <typename ParseContext> constexpr auto parse(ParseContext& ctx) {
       return ctx.begin();
     }
 
-    template <typename FormatContext> auto format(generator::format::as_literal const& text, FormatContext& ctx) {
+    template <typename FormatContext>
+    auto format(generator::format::as_compiler_message const& text, FormatContext& ctx) {
       auto out = ctx.out();
 
+      bool indent{ false };
+
       for (auto const& ch : text.str) {
+        if (indent) {
+          out    = base.format(' ', ctx);
+          out    = base.format(' ', ctx);
+          out    = base.format(' ', ctx);
+          out    = base.format(' ', ctx);
+          out    = base.format(' ', ctx);
+          out    = base.format(' ', ctx);
+          out    = base.format('|', ctx);
+          out    = base.format(' ', ctx);
+          indent = false;
+        }
+
         switch (ch) {
         case '\n':
-          out = base.format('\\', ctx);
-          out = base.format('n', ctx);
+          out    = base.format('\\', ctx);
+          out    = base.format('n', ctx);
+          indent = true;
           break;
         case '\r':
           break;
@@ -48,24 +59,6 @@ namespace fmt {
           break;
         }
       }
-
-      return out;
-    }
-
-  private:
-    formatter<char> base;
-  };
-
-  template <> struct formatter<generator::format::uppercase> {
-    template <typename ParseContext> constexpr auto parse(ParseContext& ctx) {
-      return ctx.begin();
-    }
-
-    template <typename FormatContext> auto format(generator::format::uppercase const& text, FormatContext& ctx) {
-      auto out = ctx.out();
-
-      for (auto const& ch : text.str)
-        out = base.format(static_cast<decltype(ch)>(std::toupper(static_cast<unsigned char>(ch))), ctx);
 
       return out;
     }
--- generator/core/src/generator/output.cpp
+++ generator/core/src/generator/output.cpp
@@ -160,7 +160,7 @@ namespace generator::output {
 
   */
 
-  void print(std::ostream& out, output::header header) {
+  void print(std::ostream& out, output::header) {
     format::print(out,
                   R"_(// DO NOT EDIT: this file is generated automatically
 
@@ -170,61 +170,86 @@ namespace {{ using dummy = int; }}
 #define STRINGIFY(x)   __STRINGIFY(x)
 #define PRAGMA(x)      _Pragma(#x)
 
-#if defined __GNUC__
-#define FEEDBACK_RESPONSE_ERROR(id, msg)   PRAGMA(GCC error "feedback " STRINGIFY(id) ": " msg)
-#define FEEDBACK_RESPONSE_WARNING(id, msg) PRAGMA(GCC warning "feedback " STRINGIFY(id) ": " msg)
-#define FEEDBACK_RESPONSE_MESSAGE(id, msg) PRAGMA(message "feedback " STRINGIFY(id) ": " msg)
-#define FEEDBACK_RESPONSE_NONE(id, msg)    /* no feedback response for id */
-#elif defined _MSC_VER
-#define FEEDBACK_MESSAGE(msg)              PRAGMA(message(__FILE__ "(" STRINGIFY(__LINE__) "): " msg))
-#define FEEDBACK_RESPONSE_ERROR(id, msg)   FEEDBACK_MESSAGE("feedback error " STRINGIFY(id) ": " msg)
-#define FEEDBACK_RESPONSE_WARNING(id, msg) FEEDBACK_MESSAGE("feedback warning " STRINGIFY(id) ": " msg)
-#define FEEDBACK_RESPONSE_MESSAGE(id, msg) FEEDBACK_MESSAGE("feedback message " STRINGIFY(id) ": " msg)
-#define FEEDBACK_RESPONSE_NONE(id, msg)    /* no feedback response for id */
-#else
-#error "Unsupported compiler"
-#endif
-
 )_");
-
-    auto const& workflow = header.shared_workflow.get();
-
-    for (auto [id, rule] : header.shared_rules.get())
-      format::print(out,
-                    R"_(#define FEEDBACK_MATCH_{uppercase_id}(match, highlighting) FEEDBACK_RESPONSE_{response}({id}, "{summary} [{type} from file://{origin}]\n |\n | " match "\n | " highlighting "\n |\n | RATIONALE : {rationale}\n | WORKAROUND: {workaround}\n |")
-)_",
-                    "origin"_a = format::as_literal{ header.rules_origin.generic_u8string() }, "id"_a = id,
-                    "uppercase_id"_a = format::uppercase{ id },
-                    "response"_a     = format::uppercase{ to_string(workflow[rule.type].response) },
-                    "type"_a = format::as_literal{ rule.type }, "summary"_a = format::as_literal{ rule.summary },
-                    "rationale"_a = format::as_literal{ rule.rationale }, "workaround"_a = format::as_literal{ rule.workaround });
   }
 
   void print(std::ostream& out, output::source source) {
     format::print(out, "\n#line 1 \"{}\"\n", source.filename.generic_u8string());
   }
 
-  void print(std::ostream& out, output::match match) {
-    auto const& [id, attributes] = match.rule;
+  struct location {
+    int line;
+    int column;
+  };
+
+  struct message {
+    output::location const& location;
+    std::string_view const& text;
+    text::excerpt const&    highlighting;
+  };
+
+  void print(std::ostream& out, output::message message) {
+    format::print(out,
+                  R"_(#if defined __GNUC__
+# line {line_before}
+# pragma message "{text}\n      |" \
+{indentation}"                                                                                          "
+#elif defined _MSC_VER
+# line {line}
+{indentation}PRAGMA(message(__FILE__ "(" STRINGIFY(__LINE__) "): message {text}\n      |\n{line:>5} | {match}\n      | {indentation}{annotation}"))
+#endif
+)_",
+                  "line_before"_a = message.location.line - 1, "line"_a = message.location.line,
+                  "match"_a       = format::as_compiler_message{ message.highlighting.first_line },
+                  "text"_a        = format::as_compiler_message{ message.text },
+                  "indentation"_a = message.highlighting.indentation, "annotation"_a = message.highlighting.annotation);
+  }
+
+  struct warning {
+    output::location const& location;
+    std::string_view const& text;
+    text::excerpt const&    highlighting;
+  };
+
+  void print(std::ostream& out, output::warning warning) {
 
     format::print(out,
                   R"_(#if defined __GNUC__
-# line {line}
+# line {line_before}
 # pragma GCC warning \
-{indentation}"feedback {id}: {summary} [ {type} from file://{origin} ]\n      | > rationale  > {rationale}\n      | > workaround > {workaround}\n      |"
+{indentation}"{text}\n      |                    "
 #elif defined _MSC_VER
 # line {line}
-
-{indentation}FEEDBACK_MATCH_{ID}("{match}", "{highlighting}")
+{indentation}PRAGMA(message(__FILE__ "(" STRINGIFY(__LINE__) "): warning {text}\n      |\n{line:>5} | {match}\n      | {indentation}{annotation}"))
 #endif
 )_",
-                  "line"_a = match.line_number - 1, "indentation"_a = match.highlighting.indentation,
-                  "origin"_a = match.rules_origin.generic_u8string(), "id"_a = id, "ID"_a = format::uppercase{ id },
-                  "type"_a = format::as_literal{ attributes.type }, "summary"_a = format::as_literal{ attributes.summary },
-                  "rationale"_a    = format::as_literal{ attributes.rationale },
-                  "workaround"_a   = format::as_literal{ attributes.workaround },
-                  "match"_a        = format::as_literal{ match.highlighting.first_line },
-                  "highlighting"_a = match.highlighting.indentation + match.highlighting.annotation);
+                  "line_before"_a = warning.location.line - 1, "line"_a = warning.location.line,
+                  "match"_a = format::as_compiler_message{ warning.highlighting.first_line },
+                  "text"_a = format::as_compiler_message{ warning.text }, "indentation"_a = warning.highlighting.indentation,
+                  "annotation"_a = warning.highlighting.annotation);   
+  }
+
+  struct error {
+    output::location const& location;
+    std::string_view const& text;
+    text::excerpt const&    highlighting;
+  };
+
+  void print(std::ostream& out, output::error error) {
+    format::print(out,
+                  R"_(#if defined __GNUC__
+# line {line_before}
+# pragma GCC error \
+{indentation}"{text}\n      |                    "
+#elif defined _MSC_VER
+# line {line}
+{indentation}PRAGMA(message(__FILE__ "(" STRINGIFY(__LINE__) "): error {text}\n      |\n{line:>5} | {match}\n      | {indentation}{annotation}"))
+#endif
+)_",
+                  "line_before"_a = error.location.line - 1, "line"_a = error.location.line,
+                  "match"_a       = format::as_compiler_message{ error.highlighting.first_line },
+                  "text"_a        = format::as_compiler_message{ error.text },
+                  "indentation"_a = error.highlighting.indentation, "annotation"_a = error.highlighting.annotation);
   }
 
   template <class FUNCTION>
@@ -240,9 +265,28 @@ namespace {{ using dummy = int; }}
       if (not relevant_rule_in_source_matches(line_number))
         continue;
 
+      auto const feedback = fmt::format(
+      "{id}: {summary} [ {type} from file://{origin} ]\nrationale  : {rationale}\nworkaround : {workaround}",
+      "id"_a = id, "type"_a = attributes.type, "summary"_a = attributes.summary, "rationale"_a = attributes.rationale,
+      "workaround"_a = attributes.workaround, "origin"_a = matches.rules_origin.generic_u8string());
+
       // compiler.emit_feedback (response, ...)
-      print(out, output::match{ matches.rules_origin, matches.rule, line_number,
-                                search.highlighted_text(attributes.marked_text), matches.shared_workflow });
+
+      auto const& workflow = matches.shared_workflow.get();
+      std::visit(overloaded{ [&](feedback::none) {},
+                             [&](feedback::message) {
+                               print(out, output::message{ location{ line_number, search.column() }, feedback,
+                                                           search.highlighted_text(attributes.marked_text) });
+                             },
+                             [&](feedback::warning) {
+                               print(out, output::warning{ location{ line_number, search.column() }, feedback,
+                                                           search.highlighted_text(attributes.marked_text) });
+                             },
+                             [&](feedback::error) {
+                               print(out, output::error{ location{ line_number, search.column() }, feedback,
+                                                         search.highlighted_text(attributes.marked_text) });
+                             } },
+                 workflow[attributes.type].response);
     }
   }
 
