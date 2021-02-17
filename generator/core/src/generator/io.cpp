cf2c3f46c146d8e7703c6db5a1d848d97cb68831
Dirk Reinbach <dirk.reinbach@hexagon.com>
2021-02-17 22:51:41 +0100

    Go back to standard IO streams casue memory mapped files crash in certain scenarios.

--- generator/core/src/generator/io.cpp
+++ generator/core/src/generator/io.cpp
@@ -2,12 +2,19 @@
 
 #include <mio/mmap.hpp>
 
+#include <fstream>
+#include <sstream>
+
 namespace generator::io {
   auto content(std::filesystem::path const& filename) -> std::string {
     if (!std::filesystem::exists(filename))
       throw std::invalid_argument{ "file not found" };
 
-    auto const mmap = mio::mmap_source{ filename.native() };
-    return { mmap.cbegin(), mmap.cend() };
+    //if (auto const mmap = mio::mmap_source{ filename.native() }; mmap.is_open())
+    //  return { mmap.cbegin(), mmap.cend() };
+
+    std::ostringstream content;
+    content << std::ifstream{ filename }.rdbuf();
+    return content.str();
   }
 } // namespace generator::io
